3aee50a5619e60f58798b4f04ff4bd48d418bc53
wrbernie <bernlohr.wr@gmail.com>
2022-01-18 13:59:01 -0800

    gw1 test
    test for picks from gw1

--- scrapers.py
+++ scrapers.py
@@ -27,6 +27,7 @@ def scr_player_summary(PID):
     
     Args:
         PID (int): ID for the player who will be reteieved.
+        Returns: Text file of data for a specific player.
     """
     base_URL = "https://fantasy.premierleague.com/api/element-summary/"
     full_URL = base_URL + str(PID) + "/"
@@ -42,9 +43,52 @@ def scr_player_summary(PID):
     return(data)
 
 
+def scr_team_gw(teamID,gw):
+    """This function scrapes for gw specific data for a specific team.
+    
+    Args:
+        teamID (int): ID for specific team to be colected.
+        gw (int): The desired gameweek
+    """
+    base_URL = "https://fantasy.premierleague.com/api/entry/"
+    full_URL = base_URL + str(teamID) + "/event/" + str(gw) + "/picks/"
+
+    response = ''
+    while response == '':
+        try:
+            response = requests.get(full_URL)
+        except:
+            time.sleep(5)
+    if response.status_code != 200:
+        raise Exception("Response was code " + str(response.status_code))
+    data = json.loads(response.text)
+    return(data)
+
+def scr_gw_live(gw):
+    """This function scrapes for all player data in a specific gameweek.
+
+    Args:        
+        gw(int): The gameweek that will be scraped.
+        returns: Text file with gameweek data.
+    """
+    base_URL = "https://fantasy.premierleague.com/api/event/"
+    full_URL = base_URL + str(gw) + "/live/"
+
+    response = ''
+    while response == '':
+        try:
+            response = requests.get(full_URL)
+        except:
+            time.sleep(5)
+    if response.status_code != 200:
+        raise Exception("Response was code " + str(response.status_code))
+    data = json.loads(response.text)
+    return(data)
+
 def main():
     ID = 233
-    print(scr_player_summary(ID)["history"])
+    gw = 1
+    print(scr_team_gw(ID,gw)["picks"])
     
 
 if __name__ == "__main__":
--- writers.py
+++ writers.py
@@ -9,25 +9,45 @@ import pandas as pd
 
 
 def wr_team_summary(data,basefolder):
+    """
+    Creates a csv file with team history.
+    """
     gw_history_df = pd.DataFrame.from_records(data["current"])
     gw_history_df.to_csv(os.path.join(basefolder, 'history.csv'), index=False)
 
 def wr_player_summary(data,basefolder):
+    """
+    Creates a csv file with gw-by-gw data for a specific player.
+    """
     player_summary_df = pd.DataFrame.from_records(data["history"])
     player_summary_df.to_csv(os.path.join(basefolder, 'salah.csv'), index=False)
+
+def wr_team_gw(data,basefolder,gw):
+    """
+    Creates a csv file with team data of a specific gameweek.
+    """
+    team_gw_df = pd.DataFrame.from_records(data["picks"])
+    team_gw_df.to_csv(os.path.join(basefolder, str(gw) + '.csv'), index=False)
+
+def wr_gw_data(data,basefolder,gw):
+    """creates a csv for a specific gameweek
+    """
+    gw_data_df = pd.DataFrame.from_records(data["elements"])
+    gw_data_df.to_csv(os.path.join(basefolder, 'gw' + str(gw) + '.csv'), index = False)
+
 
 
 def main():
     print('starting')
-    data = scr_player_summary(233)
+    data = scr_gw_live(1)
     
-    folder_name = "Players"
+    folder_name = "gameweeks"
     
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
         print("here")
 
-    wr_player_summary(data,folder_name)
+    wr_gw_data(data,folder_name,1)
 
     print("ending")
 
